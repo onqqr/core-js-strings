74f3928e82cf89af6ef8f33d47cc2ceaecb73726
onqqr <dnbyaniss@gmail.com>
2023-11-13 13:56:00 +0300

    feat: solved repeatSting

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -133,8 +133,11 @@ function removeTrailingWhitespaces(string) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times < 1) {
+    return '';
+  }
+  return str.repeat(times);
 }
 
 /**
