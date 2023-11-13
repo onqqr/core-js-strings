e4e782c31ae4bb4866813cf85376edd8ed621bc0
onqqr <dnbyaniss@gmail.com>
2023-11-13 13:01:44 +0300

    feat: solved isString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -37,8 +37,11 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString() {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'string' || value instanceof String) {
+    return true;
+  }
+  return false;
 }
 
 /**
