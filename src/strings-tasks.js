f3cadb9c697a8cdcb2537250f413e1176d38011c
onqqr <dnbyaniss@gmail.com>
2023-11-14 13:56:54 +0300

    feat: sloved reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -356,8 +356,11 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .reduce((acc, curr) => acc.concat(' ', reverseString(curr)), '')
+    .slice(1);
 }
 
 /**
